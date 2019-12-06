a5b136a026688e421f3d97f9fcc59c4f75a0864e
kevinmighty98 <56735050+kevinmighty98@users.noreply.github.com>
2019-12-06 23:38:40 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,33 @@
+const hexNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 const colorBtn = document.querySelector(".colorBtn");
 const chgBackground = document.querySelector("body");
+const hexColor = document.querySelector(".hex");
 
 const colors = [
   "yellow",
-  "green",
-  "blue",
-  "red",
+  "#d57eeb",
+  "#4facfe",
+  "#d4fc79",
   "#e3e3e3",
   "#fad0c4",
   "#fbc2eb"
 ];
 
-colorBtn.addEventListener("click", changeColor);
+colorBtn.addEventListener("click", getHex);
 
 function changeColor() {
   // chgBackground.style.backgroundColor = colors[2];
   let random = Math.floor(Math.random() * colors.length);
   chgBackground.style.backgroundColor = colors[random];
+}
+
+function getHex() {
+  let hexCol = "#";
+
+  for (let i = 0; i < 6; i++) {
+    let random = Math.floor(Math.random() * hexNumbers.length);
+    hexCol += hexNumbers[random];
+  }
+  chgBackground.style.backgroundColor = hexCol;
+  hexColor.innerHTML = hexCol;
 }
